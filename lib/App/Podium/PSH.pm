27ad6d97cbd37d54726195cbb5b56957f0e4329d
Andy Lester <andy@petdance.com>
2010-06-11 14:11:11 -0500

    shuffling

---
+++ lib/App/Podium/PSH.pm
@@ -0,0 +1,28 @@
+package App::Podium::PSH;
+
+# Subclassing Pod::Simple::HTML:
+# http://search.cpan.org/~arandal/Pod-Simple-3.05/lib/Pod/Simple/Subclassing.pod
+
+#use Pod::Simple::Debug(5);
+use base 'Pod::Simple::HTML';
+
+sub new {
+    my $self = shift->SUPER::new(@_);
+
+    my @passthru = qw( P M ); # P = Perldoc, M = Module
+    $self->accept_codes( @passthru );
+
+    my $tagmap = $self->{Tagmap};
+
+    for my $code ( @passthru ) {
+        $tagmap->{$code} = "$code<";
+        $tagmap->{"/$code"} = ">";
+    }
+
+    $tagmap->{'VerbatimFormatted'} =
+        qq{\n<pre class="prettyprint lang-perl">\n};
+
+    return $self;
+}
+
+1;
